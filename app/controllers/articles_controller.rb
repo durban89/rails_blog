269fd4f222947955630c497487d2e8c3d1ea560d
durban.zhang <zhangdapeng89@126.com>
2017-02-06 22:16:48 +0800

    rails render

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,13 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
   end
 
+  def destory
+		@article = Article.find(params[:id])
+		@article.destory
+
+		redirect_to articles_path
+	end
+
   def create
     #@article = Article.new(params[:article]).permit(:title, :text)
     #@article = Article.new(params.require(:article).permit(:title, :text))
